65f19cdcad7b03922b0c485b469fa87803a4abbb
Cleberson <pauluci.cleberson@gmail.com>
2024-02-09 19:37:41 -0300

    Initialized with static calc to explam the go type

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello World!")
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+
+	var futureValue = investmentAmount * (1 + expectedReturnRate/years)
+
 }
