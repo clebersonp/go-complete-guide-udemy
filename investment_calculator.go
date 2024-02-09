4465d3feee804c2494cf1dcd5b360cd1b0117df4
Cleberson <pauluci.cleberson@gmail.com>
2024-02-09 20:03:00 -0300

    Added types to variable declarations explicitly. So, we don't need casting anymore

--- investment_calculator.go
+++ investment_calculator.go
@@ -13,11 +13,11 @@ func main() {
 	// string = ""
 	// bool = false
 
-	var investmentAmount = 1000
+	var investmentAmount float64 = 1000
 	var expectedReturnRate = 5.5
-	var years = 10
+	var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Println(futureValue)
 
 	// to run the program with a module: go run .
