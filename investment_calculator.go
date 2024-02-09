11953038825e7a7fce3574b2eb1b81380db50bee
Cleberson <pauluci.cleberson@gmail.com>
2024-02-09 19:57:17 -0300

    Added the println function from fmt package to print the result

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,12 +1,25 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
+
+	// go "null types" (default values)
+	// int = 0
+	// float - 0.0
+	// string = ""
+	// bool = false
+
 	var investmentAmount = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
 
 	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	fmt.Println(futureValue)
+
+	// to run the program with a module: go run .
 
 }
