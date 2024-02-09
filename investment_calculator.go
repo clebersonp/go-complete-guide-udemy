7b5de24447ec0490cde4d62acbbe603112e6213d
Cleberson <pauluci.cleberson@gmail.com>
2024-02-09 20:15:44 -0300

    Declaring variables with explicit types and the short way

--- investment_calculator.go
+++ investment_calculator.go
@@ -13,9 +13,13 @@ func main() {
 	// string = ""
 	// bool = false
 
+	//var expectedReturnRate, years = 5.5, 10 // declared and assignment in short way using infer with keyword var
+	//expectedReturnRate, years := 5.5, 10 // declared and assignment in short way using infer without keyword var
+	//var expectedReturnRate, years float64 = 5.5, 10 // declared and assignment in short way using infer and explicit with keyword var
+	//var expectedReturnRate int, years float64 = 5.5, 10 // don't compile, go does not allow to declare multiple variables in line with multiple explicit type, only the last is allowed
 	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	expectedReturnRate := 5.5
+	years := 10.0
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	fmt.Println(futureValue)
