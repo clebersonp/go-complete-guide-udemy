79f94c97f62eeffcf641b98466807bcfffe73bb5
Cleberson <pauluci.cleberson@gmail.com>
2024-02-09 21:18:44 -0300

    Geting more values from terminal

--- investment_calculator.go
+++ investment_calculator.go
@@ -21,12 +21,20 @@ func main() {
 	//var expectedReturnRate, years float64 = 5.5, 10 // declared and assignment in short way using infer and explicit with keyword var
 	//var expectedReturnRate int, years float64 = 5.5, 10 // don't compile, go does not allow to declare multiple variables in line with multiple explicit type, only the last is allowed
 	var investmentAmount float64
-	expectedReturnRate := 5.5
-	years := 10.0
+	var expectedReturnRate float64 // when declare a variable without assignment, it needs to use keyword var and explicitly type
+	var years float64
 
-	//
-	fmt.Println("Give the investment Amount:")
+	// value for investment amount
+	fmt.Print("Give the investment Amount: ")
 	fmt.Scan(&investmentAmount) // & this is the pointer to access and change value of variable by its memory address
+
+	// value for expected return rate
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	// value for years
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
