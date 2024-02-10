9af306c7c744ac735e4e37d932ebad58708df638
Cleberson <pauluci.cleberson@gmail.com>
2024-02-09 21:07:54 -0300

    Using function Scan from fmt package to get terminal values

--- investment_calculator.go
+++ investment_calculator.go
@@ -20,9 +20,13 @@ func main() {
 	//expectedReturnRate, years := 5.5, 10 // declared and assignment in short way using infer without keyword var
 	//var expectedReturnRate, years float64 = 5.5, 10 // declared and assignment in short way using infer and explicit with keyword var
 	//var expectedReturnRate int, years float64 = 5.5, 10 // don't compile, go does not allow to declare multiple variables in line with multiple explicit type, only the last is allowed
-	var investmentAmount float64 = 1000
+	var investmentAmount float64
 	expectedReturnRate := 5.5
 	years := 10.0
+
+	//
+	fmt.Println("Give the investment Amount:")
+	fmt.Scan(&investmentAmount) // & this is the pointer to access and change value of variable by its memory address
 
 	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
